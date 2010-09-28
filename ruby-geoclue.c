1f984a0d078ec5d29fdb1212c4157d4f03ba0f38
Hermann Loose <hermannloose@gmail.com>
2010-09-28 23:44:32 +0200

    Rough wrapper for the Geoclue Master API.

--- ruby-geoclue.c
+++ ruby-geoclue.c
@@ -4,11 +4,25 @@
 #include <ruby.h>
 
 static VALUE rb_mGeoclue = Qnil;
+static VALUE rb_cGeoclueMaster = Qnil;
+static VALUE rb_cGeoclueMasterClient = Qnil;
 static VALUE rb_cGeocluePosition = Qnil;
 static VALUE rb_cGeoclueProvider = Qnil;
 
+static VALUE master_get_default()
+{
+	return Data_Wrap_Struct(rb_cGeoclueMaster, NULL, NULL, geoclue_master_get_default());
+}
+
+static VALUE master_create_client()
+{
+	return Data_Wrap_Struct(rb_cGeoclueMasterClient, NULL, NULL, geoclue_master_create_client(geoclue_master_get_default(), NULL, NULL));
+}
+
 void Init_geoclue()
 {
+	g_type_init();
+
 	rb_mGeoclue = rb_define_module("Geoclue");
 
 	rb_define_const(rb_mGeoclue, "ACCURACY_NONE", INT2FIX(GEOCLUE_ACCURACY_LEVEL_NONE));
@@ -24,7 +38,16 @@ void Init_geoclue()
 	rb_define_const(rb_mGeoclue, "STATUS_ACQUIRING", INT2FIX(GEOCLUE_STATUS_ACQUIRING));
 	rb_define_const(rb_mGeoclue, "STATUS_AVAILABLE", INT2FIX(GEOCLUE_STATUS_AVAILABLE));
 
+	rb_cGeoclueMaster = rb_define_class_under(rb_mGeoclue, "Master", rb_cObject);
+
+	rb_define_module_function(rb_cGeoclueMaster, "get_default", master_get_default, 0);
+
+	rb_define_method(rb_cGeoclueMaster, "create_client", master_create_client, 0);
+
+	rb_cGeoclueMasterClient = rb_define_class_under(rb_mGeoclue, "MasterClient", rb_cObject);
+
 	rb_cGeoclueProvider = rb_define_class_under(rb_mGeoclue, "Provider", rb_cObject);
+
 	rb_cGeocluePosition = rb_define_class_under(rb_mGeoclue, "Position", rb_cGeoclueProvider);
 
 }
